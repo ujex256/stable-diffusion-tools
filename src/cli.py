32a829ee69a94bd6a098e046e9bb22228ffc1456
ujex256 <105550500+ujex256@users.noreply.github.com>
2023-12-11 21:11:55 +0900

    docs: helpを追加

--- src/cli.py
+++ src/cli.py
@@ -17,18 +17,21 @@ class MainCLI:
         self.add_parser()
 
     def add_parser(self) -> None:
-        self.actparser = self.parser.add_subparsers(title="action", description="Do action", required=True)
+        self.actparser = self.parser.add_subparsers(title="action", description="動作", required=True)
 
         dw = self.actparser.add_parser("download", aliases=["dw", "dwm"], help="モデルをダウンロードします。")
         dw.set_defaults(func=self.download_model)
 
         conf_p = self.actparser.add_parser("config")
-        conf_p.add_argument("--reset", help="Configをリセットします。")  # dummy arg
+        conf_p.add_argument("--reset", action="store_true", help="Configをリセットします。")  # dummy arg
         conf_p.set_defaults(func=self.print_config)
 
         im_info = self.actparser.add_parser("img-info")
-        im_info.add_argument("filename", nargs="?", default=None, type=Path)
-        im_info.add_argument("--all-show", "--verbose", action="store_true")
+        im_info.add_argument(
+            "filename", nargs="?", default=None, type=Path,
+            help="ファイルのPathを指定してください。指定されなかった場合はプロンプトが表示されます。"
+        )
+        im_info.add_argument("--all-show", "--verbose", action="store_true", help="すべての要素を表示します。")
         im_info.set_defaults(func=self.show_img_info)
 
     def parse(self) -> None:
